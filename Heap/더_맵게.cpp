9635f0d327822a71dd5c7f1505d5bafd744b0812
박철준 <56952340+cheoljun99@users.noreply.github.com>
2024-04-03 00:37:57 +0900

    [feat]:2023.03.27

--- Heap/더_맵게.cpp
+++ Heap/더_맵게.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) {
     
-    
+     
     int answer = 0;
     
     priority_queue <int, vector<int>, greater<int>> pq (scoville.begin(),scoville.end());
