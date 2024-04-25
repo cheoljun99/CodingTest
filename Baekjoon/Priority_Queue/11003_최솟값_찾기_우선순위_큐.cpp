c22fe3bfa88eb6a25ec3ce1dbbb14c1aa95393fa
cheoljun99 <cheoljun99@naver.com>
2024-04-25 13:09:20 +0900

    [feat]:2024.04.25

---
+++ Baekjoon/Priority_Queue/11003_최솟값_찾기_우선순위_큐.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N, L;
+
+	cin >> N >> L;
+	//{ÀÎµ¦½º, °ª} ÇÑ±Û ±úÁü È®ÀÎ
+	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+	for (int i = 1; i <= N; i++) {
+		int num;
+		cin >> num;
+		
+		pq.push({num,i});
+
+		while (pq.top().second < i - L + 1) {
+			pq.pop();
+		}
+		cout << pq.top().first << " ";
+	}
+}
