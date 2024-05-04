d2524b4116c008cdf5d655e7251c76cf0cfc8e95
cheoljun99 <cheoljun99@naver.com>
2024-05-05 01:17:54 +0900

    [feat]:2024.05.05

--- Baekjoon/Backtracking/15655_N과_M_(6)_방법2.cpp
+++ Baekjoon/Backtracking/15655_N과_M_(6)_방법2.cpp
@@ -5,24 +5,20 @@ using namespace std;
 int N, M;
 int arr_main[8];// 주 수열
 int arr_sub[8]; // 보조 수열
-bool used[8];
 
-void backtracking(int k) {
+void backtracking(int cur, int depth) {
 
-	if (k == M) {
+	if (depth == M) {
 		for (int i = 0; i < M; i++) {
 			cout << arr_main[arr_sub[i]] << ' ';
 		}
 		cout << '\n';
 		return;
 	}
-	for (int i = 0; i <N; i++) {
-		if (!used[i]) {
-			arr_sub[k] = i;
-			used[i] = 1;
-			backtracking(k + 1);
-			used[i] = 0;
-		}
+	for (int i = cur + 1; i <= N; i++) {
+		arr_sub[depth] = i - 1;
+		backtracking(i, depth + 1);
+
 	}
 }
 
@@ -35,5 +31,5 @@ int main() {
 	for (int i = 0; i < N; i++)cin >> arr_main[i];
 	sort(arr_main, arr_main + N);
 
-	backtracking(0);
+	backtracking(0, 0);
 }
