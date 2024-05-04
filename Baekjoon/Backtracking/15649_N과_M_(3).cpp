74691473e936112a97da2dfdb07994b75c8e6449
cheoljun99 <cheoljun99@naver.com>
2024-05-04 19:57:22 +0900

    [feat]:2024.05.04

---
+++ Baekjoon/Backtracking/15649_N과_M_(3).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int N, M;
+
+int arr[8];
+void backtracking(int idx) {
+	if (idx == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		arr[idx]=i;
+		backtracking(idx+1);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	backtracking(0);
+
+}
