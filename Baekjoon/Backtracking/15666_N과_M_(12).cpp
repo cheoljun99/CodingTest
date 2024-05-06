809868b162b8cd0a0a0786bcd17ba58a9b5232b9
cheoljun99 <cheoljun99@naver.com>
2024-05-06 14:39:41 +0900

    [feat]: 2024.05.06 - 3

---
+++ Baekjoon/Backtracking/15666_N과_M_(12).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int N, M;
+
+int arr_main[8];// 주 수열
+int arr_ans[8]; // 정답 수열
+
+void backtracking(int cur,int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr_ans[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	int tmp = 0;  // 중복 수열인지 확인 하기 위해 필요한 임시 변수
+	for (int i = cur; i < N; i++) {
+		if (tmp != arr_main[i]) {
+			arr_ans[depth] = arr_main[i];
+			tmp = arr_ans[depth];
+			backtracking(i,depth + 1);
+		}
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)cin >> arr_main[i];
+	sort(arr_main, arr_main + N);
+
+	backtracking(0,0);
+
+}
