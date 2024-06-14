90c6ed0a034454485843730c4ef7206bd8e674c8
cheoljun99 <cheoljun99@naver.com>
2024-06-14 09:22:04 +0900

    [feat]: 2024.06.14-1

---
+++ Baekjoon/Longest_Increasing_Subsequence/LIS_DP_N^2/11722_가장_긴_감소하는_부분_수열.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<int> vec(N);
+	vector<int> DP(N);//Dynamic Programing O(N*N)
+
+	for (int i = 0; i < N; i++) {
+		cin >> vec[i];
+		DP[i] = 1;
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (vec[i] < vec[j]) {
+				DP[i] = max(DP[i], DP[j] + 1);
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < N; i++) ans = max(ans, DP[i]);
+	cout << ans;
+	return 0;
+
+}
