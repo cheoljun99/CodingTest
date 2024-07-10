ad0d0312eaae4a2c0aea37f18a55f58f01b899c9
cheoljun99 <cheoljun99@naver.com>
2024-07-10 09:15:25 +0900

    [feat]: 2024.07.10-1

---
+++ Baekjoon/implementation/2743_단어_길이_재기.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string str;
+	cin >> str;
+	cout << str.size();
+
+
+	
+	return 0;
+
+}
+
