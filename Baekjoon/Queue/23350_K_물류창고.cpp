a7e7e6eb744793bf7899258dbe4c83a50ef5524c
cheoljun99 <cheoljun99@naver.com>
2025-01-22 01:17:14 +0900

    [feat]:2025.01.22-1

--- Baekjoon/Queue/23350_K_물류창고.cpp
+++ Baekjoon/Queue/23350_K_물류창고.cpp
@@ -17,62 +17,72 @@ int main()
 
 	vector<int>vec(N+1);
 
+	int check = 0;
 	for (int i = 0; i < N; i++) {
 		int P, W;
 		cin >> P >> W;
 		q.push({ P,W });
 		vec[P]++;
+		if (check < P)
+			check = P;
 	}
 
-	int check = M;
+
 	int count = 0;
 	int res = 0;
 	while (!q.empty()) {
 		
-		if (q.front().first == M&&count<vec[M]) {
+		if (q.front().first == check &&vec[check]>0) {
 			if (st.empty()) {
+				//cout<<"비용발생 일" << q.front().second << "\n";
 				res += q.front().second;
 				st.push(q.front());
 				q.pop();
 			}
 			else if(q.front().first<st.top().first) {
+				//cout << "비용발생 이" << q.front().second << "\n";
 				res += q.front().second;
 				st.push(q.front());
 				q.pop();
 			}
 			else if (q.front().first == st.top().first) {
-				if (q.front().second < st.top().second) {
+				if (q.front().second <= st.top().second) {
+					//cout << "비용발생 삼" << q.front().second << "\n";
 					res += q.front().second;
 					st.push(q.front());
 					q.pop();
 				}
 				else {
-					while (!st.empty() && q.front().first == st.top().first) {
+					while (!st.empty() && q.front().first == st.top().first&& q.front().second > st.top().second) {
+						//cout << "비용발생 사" << st.top().second << "\n";
 						res += st.top().second;
 						rm.push(st.top());
 						st.pop();
 					}
+					//cout << "비용발생 오" << q.front().second << "\n";
 					res += q.front().second;
 					st.push(q.front());
 					q.pop();
 					while (!rm.empty()) {
+						//cout << "비용발생 육" << rm.top().second << "\n";
 						res += rm.top().second;
 						st.push(rm.top());
 						rm.pop();
 					}
 				}
 			}
-			count++;
-			if (count == vec[M]) {
-				count = 0;
-				M--;
+			vec[check]--;
+			if (vec[check] == 0) {
+				check--;
 			}
 		}
-		else if (q.front().first != M && count < vec[M]) {
+		else if (q.front().first != check) {
+			//cout << "비용발생 칠" << q.front().second << "\n";
 			res += q.front().second;
 			q.push(q.front());
 			q.pop();
 		}
 	}
+
 	cout << res;
 }
