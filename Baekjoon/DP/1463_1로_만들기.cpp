d69bcf23b29ccfd54bf4222515c7ebff2c544efe
cheoljun99 <cheoljun99@naver.com>
2025-02-03 19:30:09 +0900

    [feat]:2025.02.03-1 DP

---
+++ Baekjoon/DP/1463_1로_만들기.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	int N;
+	cin >> N;
+
+	vector<int>dpVector(N+1, 0);
+
+	dpVector[1] = 0;
+
+	for (int i = 2; i <= N; i++) {
+		dpVector[i] = dpVector[i-1] + 1;
+		if (i % 2 == 0)
+			dpVector[i] = min(dpVector[i], dpVector[i / 2] + 1);
+		if (i % 3 == 0)
+			dpVector[i] = min(dpVector[i], dpVector[i / 3] + 1);
+	}
+	cout << dpVector[N] << '\n';
+
+}
