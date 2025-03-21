80288eef763503383fc88622607b64f8286b892a
cheoljun99 <cheoljun99@naver.com>
2025-03-21 21:51:38 +0900

    [feat]: 2025.03.21-1 String

---
+++ Baekjoon/String/11719_그대로_출력하기_2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string str;
+
+	while (getline(cin, str)) {
+		cout << str<<'\n';
+	}
+
+}
