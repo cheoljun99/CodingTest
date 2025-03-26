64e3148753c9a70ba19079795c2ef7c21648636b
cheoljun99 <cheoljun99@naver.com>
2025-03-26 22:39:13 +0900

    [feat]: 2025.03.26-1 String

---
+++ Baekjoon/String/4999_아!.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	string str1;
+	string str2;
+	cin >> str1 >> str2;
+
+	if (str1.size() >= str2.size()) {
+		cout << "go";
+	}
+	else {
+		cout << "no";
+	}
+
+
+}
