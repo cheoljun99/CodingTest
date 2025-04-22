bc7a55cf23799396d9d5574b0f4d5d5fcc58f94e
cheoljun99 <cheoljun99@naver.com>
2025-04-22 23:11:14 +0900

    [feat]:2025.04.22 - 1 Implementation

---
+++ Baekjoon/implementation/15963_CASIO.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string N, M;
+	cin >> N >> M;
+
+	if (N == M) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+}
