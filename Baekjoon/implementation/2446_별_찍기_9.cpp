efda9894199f10f4579cdf3cec52f526bb19b46a
cheoljun99 <cheoljun99@naver.com>
2025-05-04 18:00:00 +0900

    [feat]: 2025.05.04 - 1 Implementation

---
+++ Baekjoon/implementation/2446_별_찍기_9.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+
+	for (int i = 0; i <N; i++) {
+
+		
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+
+		for (int j = 1; j <= (N * 2 - (i * 2 + 1)); j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+
+	for (int i = 1; i < N; i++) {
+
+		for (int j = 1; j <= N - i - 1; j++) {
+			cout << " ";
+		}
+
+		for (int j = 1; j <= (i * 2 + 1); j++) {
+			cout << "*";
+		}
+		cout << '\n';
+
+	}
+
+}
